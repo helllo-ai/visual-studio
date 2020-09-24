e0b43fc510b04c5029902bd71377d83540529259
helllo-ai <71385601+helllo-ai@users.noreply.github.com>
2020-09-24 08:20:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var car ,wall
+var speed,wieght
+
+
+function setup() {
+  createCanvas(1600,400);
+  car =createSprite(50, 200, 50, 50);
+  car .velocityX=speed;
+  car.shapeColor="red"
+  wall=createSprite(1500,200,60,height/2)
+  speed=random(55,90)
+  wieght=random(400,1500)
+}
+
+function draw() {
+  if (wall.x-car.x<(car.width+wall.width)/2)
+  {
+car.velocityX=0;
+var deformation=0.5*wieght*speed*speed/22509;
+if(deformation>180)
+{
+car.shapeColor=color(225,0,0);
+}
+if(deformation<180 && deformation>100)
+{
+  car.shapeColor=color(230,230,0)
+}
+if(deformation< 100)
+{
+  car.shapeColor=color(0,255,0)
+}
+  }
+  background(255,255,255);  
+  drawSprites();
+}
